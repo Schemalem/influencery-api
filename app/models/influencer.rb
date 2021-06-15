f326d0f45cf715a95302a4e5c921164f75b7cc66
Nicole Schembri <nschembri93@gmail.com>
2021-06-15 22:49:53 +0100

    Associations fixed, some validations added

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -1,3 +1,8 @@
 class Influencer < ApplicationRecord
   belongs_to :platform
+  has_many :influencer_tags
+  has_many :tags, through: :influencer_tags
+
+  validates :handle, :followers, :profile_pic_url, presence: true
+  validates :handle, uniqueness: { scope: :platform_id, message: "handle for this plaform is already logged" }
 end
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,3 +1,6 @@
 class Platform < ApplicationRecord
   has_many :influencers
+
+  validates :name, :base_url, presence: true
+  validates :name, :base_url, uniqueness: true
 end
