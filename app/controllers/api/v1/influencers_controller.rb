5cc139180e2955c8d169418ed19c2ba9b0b70d3f
Nicole Schembri <nschembri93@gmail.com>
2021-06-16 20:28:31 +0100

    Active Model Serializer action added for index of influencers

--- app/controllers/api/v1/influencers_controller.rb
+++ app/controllers/api/v1/influencers_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::InfluencersController < ApplicationController
+  #Active Model Serializer - Index action added
   def index
     @influencers = Influencer.all
     render json: @influencers
